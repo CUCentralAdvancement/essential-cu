9fb3af5375c14c523e07bb6a27079a1a4b7eaa6e
Alex Finnarn <alexfinnarn@gmail.com>
2020-08-17 13:17:28 -0400

    try taking out redirect

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/impact-reports/onward",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/impact-reports/onward",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
