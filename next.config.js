68ee8f3e6a2d023db517279d838dedc6485a9717
Alex Finnarn <alexfinnarn@gmail.com>
2020-08-17 11:58:45 -0400

    redirect base path to onward

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/impact-reports/onward",
+        permanent: true,
+      },
+    ];
+  },
+};
--- pages/impact-reports/onward/index.js
+++ pages/impact-reports/onward/index.js
@@ -3,14 +3,16 @@ import Layout from "../../../components/impact-reports/onward/global/Layout";
 
 export default function Home() {
   return (
-    <Layout>
+    <>
       <Head>
         <title>Onward</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div style={{ margin: "0 auto", maxWidth: "1280px", padding: "2rem" }}>
-        <h1>Welcome to IR20!</h1>
-      </div>
-    </Layout>
+      <Layout>
+        <div style={{ margin: "0 auto", maxWidth: "1280px", padding: "2rem" }}>
+          <h1>Welcome to IR20!</h1>
+        </div>
+      </Layout>
+    </>
   );
 }
