b168a61a2636f9cd8e3fde6ee7f53afd45761b3f
Kevin   Reynen <kevin.reynen@cu.edu>
2020-08-21 12:36:49 -0600

    removing GTM code

--- components/impact-reports/onward/global/Header.js
+++ components/impact-reports/onward/global/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function Header() {
   return (
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFCM644" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <header
       style={{
         backgroundColor: "#ccc",
