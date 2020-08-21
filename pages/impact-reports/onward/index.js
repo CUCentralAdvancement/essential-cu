5cdb93940f8fcabe113660f5f19aa18f803e5d39
Kevin   Reynen <kevin.reynen@cu.edu>
2020-08-21 09:15:52 -0600

    adding GTM

--- pages/impact-reports/onward/index.js
+++ pages/impact-reports/onward/index.js
@@ -8,6 +8,13 @@ export default function Home({ stories }) {
       <Head>
         <title>Onward</title>
         <link rel="icon" href="/favicon.ico" />
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l] = w[l] || []{'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KFCM644');</script>
+        <!-- End Google Tag Manager -->
       </Head>
       <Layout>
         <div style={{ margin: "0 auto", maxWidth: "1280px", padding: "2rem" }}>
