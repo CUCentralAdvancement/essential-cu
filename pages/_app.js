7937b44dba7a29adff79937efd43b66c54154b24
Kelly McCormack <kelly@voltagead.com>
2020-09-11 14:06:03 -0700

    Rework with global SCSS for normal CSS inheritance etc

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import "../styles/globals.css";
-import "../styles/header.css";
+import "../styles/styles.scss";
+
 import TagManager from "react-gtm-module";
 
 const tagManagerArgs = {
