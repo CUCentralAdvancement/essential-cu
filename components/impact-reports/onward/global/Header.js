2df22c6c1d51d8ae1a2c888da7792a43190cf1e0
Kelly McCormack <kelly@voltagead.com>
2020-09-11 15:54:18 -0700

    rework breakpoint vars. desktop header responsive. React does not allow CSS sticky or fixed

--- components/impact-reports/onward/global/Header.js
+++ components/impact-reports/onward/global/Header.js
@@ -7,15 +7,17 @@ export default function Header() {
       <div className="container">
 
         <span className="site-logo--container">
-          <img src="/logo-cu-png-default2x.png" alt="University of Colorado - Advancement | CU Foundation - Boulder, Colorado Springs, Denver, Anschutz Medical Campus" className="site-logo" />
+          <a href="/">
+            <img src="/logo-cu-png-default2x.png" alt="University of Colorado - Advancement | CU Foundation - Boulder, Colorado Springs, Denver, Anschutz Medical Campus" className="site-logo" />
+          </a>
         </span>
-
+          
         <h1 className="site-title">Impact Report 2020</h1>
 
         <nav className="nav-desktop">
           <span className="nav-desktop--label">Explore our:</span>
-          <a href="/#stories" className="btn">Stories</a>
-          <a href="/financials" className="btn">Financials</a>
+          <a href="/#stories" className="btn-sm">Stories</a>
+          <a href="/financials" className="btn-sm">Financials</a>
         </nav>
 
       </div>
