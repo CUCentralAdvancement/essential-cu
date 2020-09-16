aa1c27e560677fded51bcafc372b8232ce172e77
Kelly McCormack <kelly@voltagead.com>
2020-09-16 10:58:39 -0700

    Remove _document.js override, use Next/Head for Google Font stylesheet

--- components/impact-reports/onward/global/Layout.js
+++ components/impact-reports/onward/global/Layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import PropTypes from "prop-types";
 import Header from "./Header";
 import MobileLogo from "./MobileLogo";
@@ -7,6 +8,9 @@ import Footer from "./Footer";
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet"></link>
+      </Head>
       <Header />
       <MobileLogo />
       <main>{children}</main>
