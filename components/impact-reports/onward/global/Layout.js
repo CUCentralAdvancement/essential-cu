3592d023b3ff5121822405cf99dd24816bc4f78e
Kelly McCormack <kelly@voltagead.com>
2020-09-17 20:24:45 -0700

    Mobile header issue fixes

--- components/impact-reports/onward/global/Layout.js
+++ components/impact-reports/onward/global/Layout.js
@@ -10,6 +10,7 @@ export default function Layout({ children }) {
     <>
       <Head>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet"></link>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
       <MobileLogo />
