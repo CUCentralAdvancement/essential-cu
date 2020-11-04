c61f9401ba34f6d43eb0a6c19f8997c8e5d6d49b
Alex Finnarn <alex.finnarn@gmail.com>
2020-11-04 16:38:44 -0600

    comment to trigger deploy and purge caches

--- pages/impact-reports/onward/index.js
+++ pages/impact-reports/onward/index.js
@@ -176,6 +176,7 @@ export default function Home({ storyData }) {
 export async function getStaticProps() {
   const res = await fetch(`${baseURL}/api/stories`);
   const rawStoryData = await res.json();
+  // Commenting to trigger deploy.
   // const rawStoryData = require("../../../data/stories/stories.json");
 
   const stories = formatStoriesData(rawStoryData);
