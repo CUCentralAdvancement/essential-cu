d27351312b38f92f7f9b6a042a61204e029ad480
Ian Kleats <kleatsi@CUAs-MacBook-Pro.local>
2021-03-11 16:04:10 -0700

    Change background image for video elements and fix draggable video slides

--- components/impact-reports/together/VideoPlayer.tsx
+++ components/impact-reports/together/VideoPlayer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
 
 export default function VideoPlayer({
   bgVideo,
-  autoPlay = 0,
+  autoPlay: _autoPlay = 0,
   videoControls: _videoControls = 1,
 }: {
   bgVideo: string;
@@ -14,18 +14,37 @@ export default function VideoPlayer({
   const bgVideoId = splitVideoUrl[splitVideoUrl.length - 1];
   return (
     <script className="js-story-video-template" type="text/html">
-      <ReactPlayer
+      <iframe
+        title="story video"
         className="js-story-video story-video-full"
-        style={{ pointerEvents: "auto" }}
-        url={`https://player.vimeo.com/video/${bgVideoId}`}
-        config={{
-          vimeo: {
-            playerOptions: {
-              autoplay: autoPlay === 1,
-            },
-          },
+        style={{ 
+          pointerEvents: "auto",
+          minHeight: "auto",
+          height: "56.25vw"
         }}
-      />
+        src={
+          "https://player.vimeo.com/video/" +
+          bgVideoId +
+          "?autoplay=0&title=0&controls=1&byline=0&loop=1"
+        }
+        frameBorder="0"
+        allow="autoplay; fullscreen"
+        allowFullScreen
+      ></iframe>
+      {/*
+    <ReactPlayer
+      className="js-story-video story-video-full"
+      style={{pointerEvents: 'auto'}}
+      url={`https://player.vimeo.com/video/${bgVideoId}`}
+      config={{
+          vimeo: {
+              playerOptions: {
+                  autoplay: autoPlay === 1
+              }
+          }
+      }}
+    />
+  */}
     </script>
   );
 }
