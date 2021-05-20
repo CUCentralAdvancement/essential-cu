6d5ff2f6e71bd9d48ca17e5475b00225f4493829
Ian Kleats <imkleats@gmail.com>
2021-05-20 08:46:21 -0600

    Fix redirect on 2019 impact report story page and update Chart.js dependency to v2.9.3 (#169)

--- pages/impact-reports/together/story/index.tsx
+++ pages/impact-reports/together/story/index.tsx
@@ -1,12 +1,9 @@
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-// import { useMediaQuery } from 'react-responsive';
 
 export default function Story() {
-    const router = useRouter()
     useEffect(() => {
         if (typeof window !== "undefined") {
-            router.push(`/impact-reports/together/story/innovation`)
+            window.location.replace(`https://essential.cu.edu/impact-reports/together/story/innovation`)
         }
     })
     return null;
