62dcb23010412cbbf539fa5c2114c4732cf1607f
Alex Finnarn <alex.finnarn@gmail.com>
2021-08-05 17:39:09 -0500

    do not include these changes

--- components/impact-reports/2021/NumericStat.tsx
+++ components/impact-reports/2021/NumericStat.tsx
@@ -1,4 +1,5 @@
 import Image from './Image';
+
 interface NumericStatProps {
     label: string,
     variant: string,
@@ -6,7 +7,7 @@ interface NumericStatProps {
 }
 
 export default function NumericStat({label, variant, number}: NumericStatProps) {
-    let styles = {  
+    let styles = {
         container: 'flex flex-row items-center',
         number: 'text-3xl block py-4'
     };
@@ -20,11 +21,11 @@ export default function NumericStat({label, variant, number}: NumericStatProps) 
     return (
         <div className={styles.container}>
             <div className="pr-2">
-            <Image src="/2021/media-icon.png" alt="Twitter logo" sx="h-18 p-1"/>
+                <Image src="/2021/media-icon.png" alt="Twitter logo" sx="h-18 p-1"/>
             </div>
             <div>
-            <span className={styles.number}>{number}</span>
-            <span>{label}</span>
+                <span className={styles.number}>{number}</span>
+                <span>{label}</span>
             </div>
         </div>
     );
