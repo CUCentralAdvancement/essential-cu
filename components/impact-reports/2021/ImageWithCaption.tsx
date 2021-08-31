d312bf0a2c07303e548fefd48406f1ea5b917eb6
Alex Finnarn <alex.finnarn@gmail.com>
2021-08-31 11:13:56 -0500

    make image full width in ImageWithCaption component

--- components/impact-reports/2021/ImageWithCaption.tsx
+++ components/impact-reports/2021/ImageWithCaption.tsx
@@ -7,7 +7,7 @@ interface ImageWithCaptionProps {
 export default function ImageWithCaption({src, alt, caption}: ImageWithCaptionProps) {
     return (
         <figure className="rounded">
-            <img src={src} alt={alt} className="" />
+            <img src={src} alt={alt} className="w-full" />
             <figcaption className="bg-black text-white p-4 rounded-br-3xl">{caption}</figcaption>
         </figure>
 
