4e2bbbafe3ef613691b1c368b1ef0e3c05dd4e9b
Alex Finnarn <alex.finnarn@gmail.com>
2021-09-23 10:47:48 -0500

    add stuff for homepage and initial financials page

--- components/impact-reports/joy/Section.tsx
+++ components/impact-reports/joy/Section.tsx
@@ -6,17 +6,20 @@ interface SectionProps {
 }
 
 export default function Section({children, type, sx} :SectionProps) {
-    const defaultStyles = 'grid grid-cols-1 mx-auto p-4';
+    const defaultGridStyles = 'grid grid-cols-1 mx-auto p-4';
+    const defaultFlexStyles = 'flex mx-auto p-4';
     switch (type) {
         case '2-col':
-            return <div className={`${defaultStyles} md:grid-cols-2 gap-4 ${sx}`}>{children}</div>;
+            return <div className={`${defaultGridStyles} md:grid-cols-2 gap-4 ${sx}`}>{children}</div>;
         case '2-col-33':
-            return <div className={`${defaultStyles} md:grid-cols-3 ${sx}`}>{children}</div>
+            return <div className={`${defaultGridStyles} md:grid-cols-3 ${sx}`}>{children}</div>
+        case '2-col-flex':
+            return <div className={`${defaultFlexStyles} flex-col md:flex-row ${sx}`}>{children}</div>
         case '3-col':
-            return <div className={`${defaultStyles} md:grid-cols-3 ${sx}`}>{children}</div>
+            return <div className={`${defaultGridStyles} md:grid-cols-3 ${sx}`}>{children}</div>
         case '4-col':
-            return <div className={`${defaultStyles} md:grid-cols-4 ${sx}`}>{children}</div>
+            return <div className={`${defaultGridStyles} md:grid-cols-4 ${sx}`}>{children}</div>
         default:
-            return <div className={`${defaultStyles} gap-2 ${sx}`}>{children}</div>;
+            return <div className={`${defaultGridStyles} gap-2 ${sx}`}>{children}</div>;
     }
 }
