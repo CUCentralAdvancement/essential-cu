67f592143f682a48932b7ef12a4e8e10f329a2b3
Alex Finnarn <alex.finnarn@gmail.com>
2021-09-23 18:00:02 -0500

    styling updates

--- components/impact-reports/joy/ContentListing.tsx
+++ components/impact-reports/joy/ContentListing.tsx
@@ -4,54 +4,55 @@ import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
 // import {breakpoints} from '../../../data/impact-reports/2021/base'
 
 interface ContentListingProps {
-    variant: string,
-    content: any,
-    title: string
+  variant: string,
+  content: any,
+  title: string,
+  sx?: string,
 }
 
-export default function ContentListing({variant, content, title}: ContentListingProps) {
-    // const {width} = useWindowSize();
+export default function ContentListing({variant, content, title, sx}: ContentListingProps) {
+  // const {width} = useWindowSize();
 
-    switch (variant) {
-        case '4-col--hero-middle':
-            return (
-                <div className="flex flex-col">
-                    <h2 className="block py-4">{title}</h2>
-                    <div className="grid grid-cols-4 gap-2">
-                        {content.map((el, index) => {
-                            return (
-                                <div key={index} className="flex flex-col">
-                                    <span className="text-xl">{el.title}</span>
-                                    <span>{el.subTitle}</span>
-                                </div>
-                            );
-                        })}
-                    </div>
+  switch (variant) {
+    case '4-col--hero-middle':
+      return (
+        <div className={"flex flex-col " + sx}>
+          <h2 className="block py-4">{title}</h2>
+          <div className="grid grid-cols-4 gap-2">
+            {content.map((el, index) => {
+              return (
+                <div key={index} className="flex flex-col">
+                  <span className="text-xl">{el.title}</span>
+                  <span>{el.subTitle}</span>
                 </div>
-            );
-        case  'links':
-            return (
-                <>
-                    <div className={"flex flex-row justify-between items-baseline w-full"}>
-                        <span className="text-3xl font-bold block py-4">{title}</span>
-                        {/*<FontAwesomeIcon icon={faTimes} style={{height: '24px'}}*/}
-                        {/*                 className="text-gold ml-2 cursor-pointer transform hover:scale-110"/>*/}
-                    </div>
-                    <div className="space-y-2 flex flex-col shadow-inner bg-white border-t-2 border-b-2 border-gold">
-                        {content.map((el, index) => {
-                            return (
-                                <a key={index} href={`https://giving.cu.edu${el.path}`}>
-                                    <div className="flex flex-row justify-between flex-1 p-2 hover:bg-muted-gold">
-                                        <span>{el.title}</span>
-                                        <FontAwesomeIcon icon={faArrowRight} style={{height: '24px'}}
-                                                         className="text-gold ml-2"/>
-                                    </div>
-                                </a>
-                            );
-                        })}
-                    </div>
-                </>
-            );
-    }
+              );
+            })}
+          </div>
+        </div>
+      );
+    case  'links':
+      return (
+        <div className={sx}>
+          <div className={"flex flex-row justify-between items-baseline"}>
+            <span className="text-3xl font-bold block py-4">{title}</span>
+            {/*<FontAwesomeIcon icon={faTimes} style={{height: '24px'}}*/}
+            {/*                 className="text-gold ml-2 cursor-pointer transform hover:scale-110"/>*/}
+          </div>
+          <div className="space-y-2 flex flex-col shadow-inner bg-white border-t-2 border-b-2 border-gold">
+            {content.map((el, index) => {
+              return (
+                <a key={index} href={`https://giving.cu.edu${el.path}`}>
+                  <div className="flex flex-row justify-between p-2 hover:bg-muted-gold">
+                    <span>{el.title}</span>
+                    <FontAwesomeIcon icon={faArrowRight} style={{height: '24px'}}
+                                     className="text-gold ml-2"/>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      );
+  }
 
 }
--- data/impact-reports/joy/base.tsx
+++ data/impact-reports/joy/base.tsx
@@ -1,5 +1,5 @@
-export const baseURL = 'https://the-cms-staging.herokuapp.com';
-// export const baseURL = 'http://the-cms.lndo.site';
+// export const baseURL = 'https://the-cms-staging.herokuapp.com';
+export const baseURL = 'http://the-cms.lndo.site';
 export const cacheTTL = 60 * 60 * 24;
 
 export const breakpoints = {
