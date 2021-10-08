6ebf5d4bbcd724a620c934ab761c6c5831e3d24a
Alex Finnarn <alex.finnarn@gmail.com>
2021-10-08 14:51:56 -0500

    add tailwind heights to icons

--- components/impact-reports/joy/BlockQuote.tsx
+++ components/impact-reports/joy/BlockQuote.tsx
@@ -13,7 +13,7 @@ export default function BlockQuote({sx, children}: BlockQuoteProps) {
                 className={`border-t-4 border-b-4 relative ${sx}`}
     >
       <div className="absolute left-0 -top-6 z-10 p-3" style={{color: '#4BB6AE', backgroundColor: '#F5F1E5'}}>
-        <FontAwesomeIcon icon={faQuoteLeft} style={{height: '36px'}}/>
+        <FontAwesomeIcon icon={faQuoteLeft} className={"h-9"}/>
       </div>
       <p className="px-16 py-4" style={{color: '#065E62'}}>
         {children}
