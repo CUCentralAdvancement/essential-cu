7f9be40e2261897d6cb2f29f2f4ce16078e8e9d4
Alex Finnarn <alex.finnarn@gmail.com>
2021-10-20 18:35:38 -0500

    revert errant img src changes

--- components/impact-reports/together/HtmlDocument.tsx
+++ components/impact-reports/together/HtmlDocument.tsx
@@ -52,7 +52,7 @@ export default function HtmlDocument({
         {/* Google Tag Manager /}
         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.url=
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-KFCM644')};`}}
         />
@@ -65,7 +65,7 @@ export default function HtmlDocument({
 
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe url="https://www.googletagmanager.com/ns.html?id=GTM-KFCM644"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFCM644"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>`
             }}
           />
