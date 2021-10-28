4fb19c1a2b1ef44f7fac88497fbd3f28b3107099
Alex Finnarn <alex.finnarn@gmail.com>
2021-10-28 16:06:33 -0500

    add linear gradient to financials page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     boxShadow: {
       sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
       DEFAULT: '0 2px 4px 0 rgba(0,0,0,0.5)',
+      bottom: '0 4px 4px -4px rgba(0,0,0,0.5)',
       liddle: '0 2px 6px 0 rgba(0,0,0,0.5)',
       md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
       lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
